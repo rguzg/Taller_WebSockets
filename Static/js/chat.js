196d23f6445b0169d5cfaae4da6ed94d45bfd9b6
Raúl <raulguzmanguerrero@outlook.com>
2021-03-07 21:08:53 -0600

    Add functionality to go back button

--- Static/js/chat.js
+++ Static/js/chat.js
@@ -3,6 +3,10 @@ const toggleChatMemberScreen = () => {
     document.querySelector('#chat').classList.toggle('h-display-none');
 }
 
+const goBack = () => {
+    window.location = "/";
+}
+
 document.querySelector('#open_chat_members').addEventListener('click', toggleChatMemberScreen);
 document.querySelector('#close_chat_members').addEventListener('click', toggleChatMemberScreen);
-
+document.querySelector('#back').addEventListener('click', goBack);
