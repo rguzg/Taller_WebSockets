ffd1383933144579e0ee95918e4a18f3cb67c6dc
Raúl <raulguzmanguerrero@outlook.com>
2021-03-10 13:00:41 -0600

    Add WebSocket server and basic message exchanging

--- index.js
+++ index.js
@@ -1,11 +1,31 @@
 const express = require('express');
+const ws = require('ws');
+const http = require('http');
+
 const app = express();
+const server = http.createServer(app);
+const socket_server = new ws.Server({clientTracking: false, noServer: true});
 
 app.use('/static', express.static('Static'));
 
 app.get('/', (req, res) => res.sendFile(`${__dirname}/Templates/login.html`));
 app.get('/chat', (req, res) => res.sendFile(`${__dirname}/Templates/chat.html`));
 
-app.listen(process.env.PORT || 3000, () => {
+server.on('upgrade', (req, socket, head) => {
+    socket_server.handleUpgrade(req, socket, head, (socket, req)=> {
+        socket_server.emit('connection', socket, req);
+    });
+})
+
+socket_server.on('connection', (ws, request) => {  
+    ws.on('message', (message) => {
+      console.log(`Received message ${message}`);
+      ws.send(message);
+    });
+  
+    ws.send('¡Feliz Navidad! 🎄')
+  });
+
+server.listen(process.env.PORT || 3000, () => {
     console.log('The server is running!');
 });
