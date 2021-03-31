e03699af584beaed8953c46fc8079057b6dc019b
Raúl <raulguzmanguerrero@outlook.com>
2021-03-30 18:03:50 -0600

    Save JWT if response.ok is true

--- Static/js/login.js
+++ Static/js/login.js
@@ -10,18 +10,33 @@ document.querySelector('#nombre').addEventListener('input', (element) => {
     }
 });
 
+// Enviar petición a /login cuando se haga click en #empezar_chatear
 document.querySelector('#empezar_chatear').addEventListener('click', async () => {
     let username = document.querySelector('#nombre').value;
     let color = document.querySelector('#color').value;
 
-    let response = await fetch('login', {
-        method: 'POST',
-        headers: {
-            "Content-Type": "application/json"
-        },
-        body: JSON.stringify({
-            username,
-            color
-        })
-    });
+    try {
+        let response = await fetch('login', {
+            method: 'POST',
+            headers: {
+                "Content-Type": "application/json"
+            },
+            body: JSON.stringify({
+                username,
+                color
+            })
+        });
+    
+        let json = await response.json();
+    
+        // Si response.ok es verdadero, se almacena el JWT en SessionStorage. Si response.ok es falso
+        // se imprime el codigo de error en la consola
+        if(response.ok){
+            sessionStorage.setItem('token', json['message']['token']);
+        } else {
+            console.error(`${json['status']}: ${json['message']}`);
+        }
+    } catch (error) {
+        console.error(error);
+    }
 });
