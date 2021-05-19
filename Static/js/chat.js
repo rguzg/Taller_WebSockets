2f28db575417a2b42a693d7e8e85270ff4891da3
Raúl <raulguzmanguerrero@outlook.com>
2021-05-18 19:58:01 -0500

    Segunda sesión

--- Static/js/chat.js
+++ Static/js/chat.js
@@ -17,6 +17,22 @@ document.querySelector('#close_chat_members').addEventListener('click', () => {
     toggleChatMemberScreen();
 });
 
+document.querySelector('#open_chat_members').addEventListener('click', async () => {
+    let response = await fetch('/connected');
+
+    let json = await response.json();
+
+    let miembros = document.querySelector('#chat_members');
+
+    json['message'].forEach(miembro => {
+        let span = document.createElement('span');
+        span.textContent = miembro;
+        span.style.color = "#ffffff";
+
+        miembros.appendChild(span);
+    });
+});
+
 function CrearMensaje(mensaje, color, nombre_usuario) {
     let contenedor_mensaje = document.createElement('div');
     let username = document.createElement('span');
@@ -41,30 +57,99 @@ function CrearMensaje(mensaje, color, nombre_usuario) {
 
 document.querySelector('#enviar_mensaje').addEventListener('click', () => {
     let mensaje = document.querySelector('#chat_text-box').value;
+    let image_input = document.querySelector('#imagen');
 
-    let json_mensaje = {
-        username: sessionStorage.getItem('username'),
-        color: sessionStorage.getItem('color'),
-        message: mensaje
+    let image = null;
+    let json_mensaje = {};
+
+    if(image_input.value){
+        let reader = new FileReader();
+        reader.readAsArrayBuffer(image_input.files[0]);
+
+        reader.onload = () => {
+            image = reader.result;
+            image = new Uint8Array(image);
+
+            json_mensaje = {
+                username: sessionStorage.getItem('username'),
+                color: sessionStorage.getItem('color'),
+                message: mensaje,
+                image
+            }
+
+            // ws.send(JSON.stringify(json_mensaje));
+            ws.emit('message', json_mensaje)
+        }
+    } else {
+        json_mensaje = {
+            username: sessionStorage.getItem('username'),
+            color: sessionStorage.getItem('color'),
+            message: mensaje,
+        }
+
+        ws.emit('message', json_mensaje);
+        // ws.send(JSON.stringify(json_mensaje));
     }
-
-    ws.send(JSON.stringify(json_mensaje));
 
     document.querySelector('#chat_text-box').value = "";
 });
 
-let ws = new WebSocket('ws://localhost:3000');
-ws.onopen = () => {
-    // ws.send("Hola Mundo");
-}
+// let ws = new WebSocket('ws://localhost:3000?number=2');
+let ws = io('wss://d2ce49d398d0.ngrok.io', {
+    auth: {
+        numero: 2
+    },
+    transports: ["websocket"]
+});
 
-ws.onmessage = (message) => {
+ws.on('connect', () => {
+    console.log("Estoy conectado");
+});
+
+ws.on('message', (message) => {
     console.log(message);
 
-    let parsed_json = JSON.parse(message.data);
+        // let parsed_json = JSON.parse(message.data);
+    
+        let nuevo_mensaje = CrearMensaje(message.message, message.color, message.username);
+    
+        let contenedor_mensajes = document.querySelector('.m-chat-box__messages');
+        
+        contenedor_mensajes.appendChild(nuevo_mensaje);
+    
+        if(message.image){
+            let blob_url = URL.createObjectURL(new Blob([Uint8Array.from(message.image)]));
+    
+            let image = document.createElement('img');
+            image.src = blob_url;
+    
+            contenedor_mensajes.appendChild(image);
+        }
+    
+});
 
-    let nuevo_mensaje = CrearMensaje(parsed_json.message, parsed_json.color, parsed_json.username);
+// ws.onopen = () => {
+//     // ws.send("Hola Mundo");
+// }
 
-    let contenedor_mensajes = document.querySelector('.m-chat-box__messages');
-    contenedor_mensajes.appendChild(nuevo_mensaje);
-}
+// ws.onmessage = (message) => {
+//     console.log(message);
+
+//     let parsed_json = JSON.parse(message.data);
+
+//     let nuevo_mensaje = CrearMensaje(parsed_json.message, parsed_json.color, parsed_json.username);
+
+//     let contenedor_mensajes = document.querySelector('.m-chat-box__messages');
+    
+//     contenedor_mensajes.appendChild(nuevo_mensaje);
+
+//     if(parsed_json.image){
+//         let blob_url = URL.createObjectURL(new Blob([Uint8Array.from(parsed_json.image)]));
+
+//         let image = document.createElement('img');
+//         image.src = blob_url;
+
+//         contenedor_mensajes.appendChild(image);
+//     }
+
+// }
--- index.js
+++ index.js
@@ -1,22 +1,92 @@
 const express = require('express');
 const app = express();
 const ws = require('ws');
+const io = require('socket.io');
 const http = require('http');
 const database = require('./database');
 
 const server = http.createServer(app);
-const socket_server = new ws.Server({server: server});
+// const socket_server = new ws.Server({noServer: true});
+const socket_server = io(server, {
+    port: 3000,
+    serveClient: false
+});
+
+let map = new Map()
+
+// Utilizando libreria ws
+// socket_server.on('connection', (socket) => {
+//     socket.on('message', (message) => {
+//         let parsed_message = JSON.parse(message);
+
+//         if(! map.has(socket)){
+//             map.set(socket, parsed_message.username);
+//         }
+
+//         console.log("Se recibió mensaje de: " + parsed_message.username + " el mensaje es: " + parsed_message.message);
+
+//         if(parsed_message['image'] != null){
+//             console.log(parsed_message['image'][0]);
+//         }
+
+//         socket_server.clients.forEach((client) => {
+//             client.send(message);
+//         })
+//     });
+
+//     socket.on('close', () => {
+//         let disconnected_user = map.get(socket);
+//         console.log(database);
+//         database.usuarios[disconnected_user] = "";
+//         console.log(database);
+//     });
+// });
 
 socket_server.on('connection', (socket) => {
     socket.on('message', (message) => {
-        let parsed_message = JSON.parse(message);
+        console.log("Entre");
 
-        console.log("Se recibió mensaje de: " + parsed_message.username + " el mensaje es: " + parsed_message.message);
+        if(! map.has(socket)){
+            map.set(socket, message.username);
+        }
 
-        socket_server.clients.forEach((client) => {
-            client.send(message);
-        })
+        console.log("Se recibió mensaje de: " + message.username + " el mensaje es: " + message.message);
+
+        if(message['image'] != null){
+            console.log(message['image'][0]);
+        }
+
+        socket_server.emit('message', message);
     });
+
+    socket.on('disconnect', () => {
+        let disconnected_user = map.get(socket);
+        console.log(database);
+        database.usuarios[disconnected_user] = "";
+        console.log(database);
+    });
+});
+
+// server.on('upgrade', (req, socket, head) => {
+//     let numbers = req.url.split("number=")[1];
+
+//     if(numbers == 2){
+//         socket_server.handleUpgrade(req, socket, head, (socket, req) => {
+//             socket_server.emit('connection', socket, req);
+//         });
+//     } else {
+//         socket.destroy();
+//     }
+// });
+
+socket_server.use((socket, next) => {
+    let numbers = socket.handshake.auth.numero;
+
+        if(numbers == 2){
+            next();
+        } else {
+            next(new Error());
+        }
 });
 
 app.use('/static', express.static('Static'));
@@ -26,9 +96,9 @@ app.get('/', (req, res) => res.sendFile(`${__dirname}/Templates/login.html`));
 app.get('/chat', (req, res) => res.sendFile(`${__dirname}/Templates/chat.html`));
 
 app.get('/connected', (req, res) => {
-    let connected_users = Object.keys(database.usuarios).filter((user) => database.usuarios[user].connected);
-
-    console.log(connected_users);
+    // let connected_users = Object.keys(database.usuarios).filter((user) => database.usuarios[user].connected);
+    let connected_users = Object.keys(database.usuarios);
+    res.status(200).json({'status': 200, 'message': connected_users});
 });
 
 app.post('/login', (req, res) => {
@@ -43,7 +113,6 @@ app.post('/login', (req, res) => {
             username,
             // username: username
             color,
-            connected: true,
         }
 
         console.log(database);
